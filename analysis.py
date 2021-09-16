c5648173133ab9010b82151d1130048e1fa17cbe
rw417 <37159376+rw417@users.noreply.github.com>
2021-09-16 14:38:55 -0400

    fixed the typo

--- analysis.py
+++ analysis.py
@@ -5,4 +5,4 @@ data = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Da
 
 mort_rate = data['Mortality rate, infant (per 1,000 live births)']
 gdp_percap = data['GDP per capita (constant 2010 US$)']
-country = data['Country Nameg']
+country = data['Country Name']
