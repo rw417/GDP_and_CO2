cd54b46d63ba6a669fa57a689c558c6dadd08b61
rw417 <37159376+rw417@users.noreply.github.com>
2021-09-18 13:02:41 -0400

    changes

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 data = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
@@ -6,3 +7,10 @@ data = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Da
 mort_rate = data['Mortality rate, infant (per 1,000 live births)']
 gdp_percap = data['GDP per capita (constant 2010 US$)']
 country = data['Country Name']
+
+plt.scatter(y=mort_rate, x=gdp_percap, alpha=0.5)
+plt.ylabel("Mortality rate, infant (per 1,000 live births)")
+plt.xlabel("GDP per capita (constant 2010 US$)")
+plt.title("Infant Mortality Rate vs GDP per Capita")
+plt.show()
+
